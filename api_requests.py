3a2bac0ccc66dba942418eb259660b91e0992767
Zebat2 <zebat2@illinois.edu>
2020-06-13 22:54:36 -0500

    Added python geocode

--- api_requests.py
+++ api_requests.py
@@ -1,4 +1,7 @@
 import requests
+from opencage.geocoder import OpenCageGeocode
+from pprint import pprint
+
 
 
 response = requests.get('https://jobs.github.com/positions.json?&full_time=true&location=gurnee')
@@ -7,3 +10,22 @@ response.raise_for_status()
 jsonResponse = response.json()
 print("Entire JSON response")
 print(jsonResponse)
+
+
+key = 'e6cdbf647b42473a82025fe1c6dbf63f'
+geocoder = OpenCageGeocode(key)
+
+results = geocoder.reverse_geocode(44.8303087, -0.5761911)
+pprint(results)
+# [{'components': {'city': 'Bordeaux',
+#                  'country': 'France',
+#                  'country_code': 'fr',
+#                  'county': 'Bordeaux',
+#                  'house_number': '11',
+#                  'political_union': 'European Union',
+#                  'postcode': '33800',
+#                  'road': 'Rue Sauteyron',
+#                  'state': 'New Aquitaine',
+#                  'suburb': 'Bordeaux Sud'},
+#   'formatted': '11 Rue Sauteyron, 33800 Bordeaux, France',
+#   'geometry': {'lat': 44.8303087, 'lng': -0.5761911}}]
