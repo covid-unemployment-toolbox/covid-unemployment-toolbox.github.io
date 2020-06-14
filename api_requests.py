fb3f63c745eea31c1795e2ea2bb98a3f4099ed0b
Zeba Tabassum <zeba4@Zeba-Tabassum.localdomain>
2020-06-13 23:35:11 -0500

    Added forward geocoding and more parsing done

--- api_requests.py
+++ api_requests.py
@@ -1,31 +1,47 @@
 import requests
 from opencage.geocoder import OpenCageGeocode
-from pprint import pprint
+# from pprint import pprint
 
+# api-endpoint
+URL = 'https://jobs.github.com/positions.json'
 
+# # location given here
+location = 'new york' #enter user inputted city
+full_time = 'true'
+description = 'engineer'
 
-response = requests.get('https://jobs.github.com/positions.json?&full_time=true&location=gurnee')
-response.raise_for_status()
+# defining a params dict for the parameters to be sent to the API
+PARAMS = {'location':location, 'full_time': full_time, 'decription':description}
+
+# sending get request and saving the response as response object
+response = requests.get(url = URL, params = PARAMS)
+
+# extracting data in json format
+# data = r.json()
+
+# response = requests.get('https://jobs.github.com/positions.json?&full_time=full_time&location=new+york&description=')
+# response.raise_for_status()
 # access JSOn content
 jsonResponse = response.json()
-print("Entire JSON response")
-print(jsonResponse)
+for i,v in enumerate(jsonResponse):
+    for key, value in jsonResponse[i].items():
+        if key == 'location':
+            print(key, ":", value)
+        if key == 'company':
+            print(key, ":", value)
+        if key == 'title':
+            print(key, ":", value)
 
 
+#Forward Geocoding
 key = 'e6cdbf647b42473a82025fe1c6dbf63f'
 geocoder = OpenCageGeocode(key)
 
-results = geocoder.reverse_geocode(44.8303087, -0.5761911)
-pprint(results)
-# [{'components': {'city': 'Bordeaux',
-#                  'country': 'France',
-#                  'country_code': 'fr',
-#                  'county': 'Bordeaux',
-#                  'house_number': '11',
-#                  'political_union': 'European Union',
-#                  'postcode': '33800',
-#                  'road': 'Rue Sauteyron',
-#                  'state': 'New Aquitaine',
-#                  'suburb': 'Bordeaux Sud'},
-#   'formatted': '11 Rue Sauteyron, 33800 Bordeaux, France',
-#   'geometry': {'lat': 44.8303087, 'lng': -0.5761911}}]
+query = u'Bosutska ulica 10, Trnje, Zagreb, Croatia'
+results = geocoder.geocode(query)
+
+print(u'%f;%f;%s;%s' % (results[0]['geometry']['lat'], 
+                        results[0]['geometry']['lng'],
+                        results[0]['components']['country_code'],
+                        results[0]['annotations']['timezone']['name']))
+# 45.797095;15.982453;hr;Europe/Belgrade
